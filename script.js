f12ac4b48f7c87d82cef5e0f20bac0212418ceb0
ShadoW <141430071+101ShadoW101@users.noreply.github.com>
2023-08-14 20:08:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// let num = 5.5
+
+// console.log(num.toFixed())
+
+
+// console.log(Math.ceil(num))
+// console.log(Math.floor(num))
+// console.log(Math.round(num))
+
+
+// console.log(Math.min(1,2,34,5))
+// console.log(Math.max(1,2,34,5))
+
+// console.log(Math.pow(4,2))
+
+
+
+let str = "HTML JavaScript Python";
+console.log(str.replaceAll(' ', '-'))
+
+
+let str1 = "string not starting with capital";  
+console.log(str1.replace('s', 'S'))
+
+let str2 = "КаЖдЫй ОхОтНиК жЕлАеТ зНаТь";
+console.log(str2.toLowerCase().replace('к', 'К'))
+
+let string = ""
+let StRiNg = ""
+console.log(string === StRiNg)
+
+
+let ABCDe = ''
+let AbcdW = ''
+
+console.log('ABCDe' === 'AbcdW')
